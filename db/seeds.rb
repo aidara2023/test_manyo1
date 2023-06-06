ce51b18a72404834483abd03a92b788ce50d169c
aidara2023 <bintou1212aidara@gmail.com>
2023-06-06 10:45:49 +0000

    manyo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |i|
+    Task.find_or_create_by (
+        title : "task#[i+1]",
+        content : "content[i*10]",
+    )
+end
