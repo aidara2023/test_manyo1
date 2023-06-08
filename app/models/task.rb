b4bbf4916af35e85587b11d1750784f6b9aa7273
aidara2023 <bintou1212aidara@gmail.com>
2023-06-08 15:18:20 +0000

    step3

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
-  validates :title, :content, presence: true
+validates :title, :content, presence: true
+validates :deadline_on, presence: true
+validates :priority, presence: true
+validates :status, presence: true
+
+enum priority: { low: 0, middle: 1, high: 2 }
+enum status: { todo: 0, doing: 1, done: 2 }
+
+scope :sort_deadline_on, -> { order(deadline_on: :asc) }
+scope :sort_priority, -> { order(priority: :desc) }
+scope :sort_created_at, -> { order(created_at: :desc) }
+
+scope :search_status, -> (status) { where(status: status) }
+scope :search_title, -> (title) { where("title LIKE ?", "%#{title}%") }
 end
