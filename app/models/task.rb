a0ca391bf31c5e2b00734d0c1695a3d5dccf7ef2
aidara2023 <bintou1212aidara@gmail.com>
2023-06-15 14:11:35 +0000

    rectification

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ validates :title, :content, presence: true
 validates :deadline_on, presence: true
 validates :priority, presence: true
 validates :status, presence: true
+belongs_to :user
 
 enum priority: { low: 0, middle: 1, high: 2 }
 enum status: { todo: 0, doing: 1, done: 2 }
